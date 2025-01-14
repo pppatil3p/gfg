0cc25191f6f8e1eeb6c6f1bcd56c8a5a86c9bac9
Prathmesh Patil <119526643+pppatil3p@users.noreply.github.com>
2025-01-14 21:24:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input1 = sc.nextLine();
+            Scanner ss1 = new Scanner(input1);
+            while (ss1.hasNextInt()) {
+                arr.add(ss1.nextInt());
+            }
+            Solution ob = new Solution();
+            int res = ob.findKRotation(arr);
+            System.out.println(res);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findKRotation(List<Integer> arr) {
+        int low = 0, high = arr.size() - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr.get(low) <= arr.get(high)) {
+                if (arr.get(low) < ans) {
+                    index = low;
+                    ans = arr.get(low);
+                }
+                break;
+            }
+            if (arr.get(low) <= arr.get(mid)) {
+                if (arr.get(low) < ans) {
+                    index = low;
+                    ans = arr.get(low);
+                }
+                low = mid + 1;
+            } else {
+                if (arr.get(mid) < ans) {
+                    index = mid;
+                    ans = arr.get(mid);
+                }
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+}
